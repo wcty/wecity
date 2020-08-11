3797cca87229cff4076a92c1d3fbcf24addbf9d7
Herman Mitish <german.mitish@gmail.com>
2020-08-12 00:47:56 +0300

    Fixed imports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App.js'
 import 'core-js'
 import 'raf/polyfill'
 import './styles/index.css'
-import * as serviceWorker from '../serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />);
 serviceWorker.unregister()
