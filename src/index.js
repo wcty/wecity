a1ade16fd0e43dcebdc24ca1358550d5e19990e1
Herman Mitish <german.mitish@gmail.com>
2020-08-12 00:35:51 +0300

    Added core.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.js'
+import 'core-js'
+import 'raf/polyfill'
 import './styles/index.css'
 
 ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />);
-//registerServiceWorker()
